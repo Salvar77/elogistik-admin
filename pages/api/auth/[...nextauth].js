72e8493bc4b9cc35945368167d0f36723b167a8f
Łukasz <lukaszkus77@gmail.com>
2024-07-03 21:25:46 +0200

    update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,8 +3,6 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth, { getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const adminEmails = ["lukaszkus77@gmail.com"];
-
 export const authOptions = {
   providers: [
     GoogleProvider({
@@ -15,11 +13,8 @@ export const authOptions = {
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
     session: ({ session, token, user }) => {
-      if (adminEmails.includes(session?.user?.email)) {
-        return session;
-      } else {
-        return false;
-      }
+      // Dodaj inne logiki, jeśli są potrzebne
+      return session;
     },
   },
 };
@@ -28,9 +23,46 @@ export default NextAuth(authOptions);
 
 export const isAdminRequest = async (req, res) => {
   const session = await getServerSession(req, res, authOptions);
-  if (!adminEmails.includes(session?.user?.email)) {
+  if (!session?.user?.email) {
     res.status(401);
     res.end();
-    throw "brak admina";
+    throw "Brak admina";
   }
 };
+
+// import clientPromise from "@/lib/db";
+// import { MongoDBAdapter } from "@auth/mongodb-adapter";
+// import NextAuth, { getServerSession } from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+
+// const adminEmails = ["lukaszkus77@gmail.com"];
+
+// export const authOptions = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID,
+//       clientSecret: process.env.GOOGLE_SECRET,
+//     }),
+//   ],
+//   adapter: MongoDBAdapter(clientPromise),
+//   callbacks: {
+//     session: ({ session, token, user }) => {
+//       if (adminEmails.includes(session?.user?.email)) {
+//         return session;
+//       } else {
+//         return false;
+//       }
+//     },
+//   },
+// };
+
+// export default NextAuth(authOptions);
+
+// export const isAdminRequest = async (req, res) => {
+//   const session = await getServerSession(req, res, authOptions);
+//   if (!adminEmails.includes(session?.user?.email)) {
+//     res.status(401);
+//     res.end();
+//     throw "brak admina";
+//   }
+// };
