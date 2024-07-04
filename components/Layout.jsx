065c1904d96acad7ffc121ea783296a854dc223f
Łukasz <lukaszkus77@gmail.com>
2024-07-04 11:31:08 +0200

    update

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,29 +1,34 @@
 import styles from "../styles/Home.module.scss";
 import Logo from "./Logo";
 import { useSession, signIn, signOut } from "next-auth/react";
-
 import Nav from "./Nav";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Layout = ({ children }) => {
   const [showNav, setShowNav] = useState(false);
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    console.log("Session data:", session);
+    console.log("Session status:", status);
+  }, [session, status]);
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
   if (!session) {
     return (
-      <>
-        <div className={styles.main}>
-          <div className={styles.main__box}>
-            <button
-              onClick={() => signIn("google")}
-              className={styles.main__btn}
-            >
-              Zaloguj do Google
-            </button>
-          </div>
+      <div className={styles.main}>
+        <div className={styles.main__box}>
+          <button onClick={() => signIn("google")} className={styles.main__btn}>
+            Zaloguj do Google
+          </button>
         </div>
-      </>
+      </div>
     );
   }
+
   return (
     <div className={styles.main__wrapper}>
       <div className={styles.main__menu}>
@@ -50,7 +55,6 @@ const Layout = ({ children }) => {
           <Logo />
         </div>
       </div>
-
       <div className={styles.main__google}>
         <Nav show={showNav} />
         <div className={styles.main__session}>{children}</div>
